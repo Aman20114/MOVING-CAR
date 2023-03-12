e1634c7f0b6970a23e809d8846b7a8240cf17d79
Aman Tiwari <110708815+Aman20114@users.noreply.github.com>
2023-03-12 19:34:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// function takeOrder(giveFood){
+//     console.log("taking order...");
+//     giveFood(takePayment, sorry);
+// }
+
+// function giveFood(takePayment, _sorry){
+//     console.log("giving food...");
+//     takePayment(tankyou);
+// }
+
+// function takePayment(thankyou){
+//     console.log("taking payment...");
+//     thankyou();
+// }
+
+// function tankyou(){
+//     console.log("Thankyou...")
+// }
+// takeOrder(giveFood);
